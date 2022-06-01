68544b8c654868656de49e7052605ba38220e079
SoKnight <mcliyanage2018@gmail.com>
2022-06-01 21:31:06 +0500

    Added feedback interceptor instance field

--- plugin/src/main/java/ru/easydonate/easypayments/exception/CommandExecutionException.java
+++ plugin/src/main/java/ru/easydonate/easypayments/exception/CommandExecutionException.java
@@ -3,6 +3,7 @@ package ru.easydonate.easypayments.exception;
 import lombok.Getter;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
+import ru.easydonate.easypayments.execution.interceptor.FeedbackInterceptor;
 
 @Getter
 public final class CommandExecutionException extends RuntimeException {
@@ -10,10 +11,12 @@ public final class CommandExecutionException extends RuntimeException {
     private static final String MESSAGE_FORMAT = "Couldn't correctly execute command '%s'";
 
     private final String command;
+    private final FeedbackInterceptor executor;
 
-    public CommandExecutionException(@NotNull String command, @NotNull Throwable cause) {
+    public CommandExecutionException(@NotNull String command, @NotNull FeedbackInterceptor executor, @NotNull Throwable cause) {
         super(String.format(MESSAGE_FORMAT, ChatColor.stripColor(command)), cause);
         this.command = command;
+        this.executor = executor;
     }
 
 }
