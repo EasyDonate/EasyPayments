35c57e4370c182dd23b28ea9888c1aba87c8ce03
SoKnight <mcliyanage2018@gmail.com>
2025-09-11 17:59:43 +0500

    build: process build platforms property

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,13 +13,60 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "EasyPayments"
 
-// --- project modules
+// -------------- PLATFORM-INDEPENDENT MODULES -------------------------------------------------------------------------
+
 include(":core")
-include(":platform:folia")              // Folia implementation
-include(":platform:paper:internals")    // Paper Internals (unrelocated CraftBukkit)
-include(":platform:paper:universal")    // Paper Universal (via API)
-include(":platform:spigot:v1")          // Spigot Internals  1.8 ~ 1.12.2
-include(":platform:spigot:v2")          // Spigot Internals 1.13 ~ 1.16.5
-include(":platform:spigot:v3")          // Spigot Internals 1.17 ~ 1.18.2
-include(":platform:spigot:v4")          // Spigot Internals 1.19 ~ latest
 include(":plugin")
+
+// -------------- OPTIONAL PLATFORM MODULES ----------------------------------------------------------------------------
+
+// populate properties for submodules
+private val buildPlatforms = System.getProperty("easypayments.build.platforms", "all").let(BuildPlatform::parse)
+BuildPlatform.entries.forEach {
+    System.setProperty(
+        "easypayments.build.platform.${it.key}",
+        buildPlatforms.contains(it).toString()
+    )
+}
+
+// Folia support module
+include(":platform:folia")
+
+// Paper Internals : unrelocated CraftBukkit package
+include(":platform:paper:internals")    // Paper 1.20.6 and newer
+
+// Paper Universal : Paper API driven implementation
+include(":platform:paper:universal")    // Paper 1.18.1 and newer
+
+// Spigot Internals : relocated CraftBukkit and NMS packages
+include(":platform:spigot:v1")          // Spigot 1.8  ~ 1.12.2
+include(":platform:spigot:v2")          // Spigot 1.13 ~ 1.16.5
+include(":platform:spigot:v3")          // Spigot 1.17 ~ 1.18.2
+include(":platform:spigot:v4")          // Spigot 1.19 and newer
+
+// -------------- INTERNAL CLASSES -------------------------------------------------------------------------------------
+
+private enum class BuildPlatform(val key: String) {
+
+    PAPER_INTERNALS     ("paper-internals"),
+    PAPER_UNIVERSAL     ("paper-universal"),
+    SPIGOT_INTERNALS    ("spigot-internals"),
+    ;
+
+    companion object {
+
+        fun parse(input: String): Set<BuildPlatform> {
+            val keys = input.split(',').map(String::lowercase)
+            if (keys.contains("all"))
+                return entries.toSet()
+
+            return buildSet {
+                keys.forEach { key ->
+                    add(entries.firstOrNull { it.key == key } ?: throw GradleException("unknown platform: '$key'"))
+                }
+            }
+        }
+
+    }
+
+}
