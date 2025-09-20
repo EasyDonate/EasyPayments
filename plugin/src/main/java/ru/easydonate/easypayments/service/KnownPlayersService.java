864cead0b03844afd9532610ad643e7a559f153d
SoKnight <mcliyanage2018@gmail.com>
2025-09-20 17:31:35 +0500

    refactor: we should weak-cache Player instances to determine online status faster

--- plugin/src/main/java/ru/easydonate/easypayments/service/KnownPlayersService.java
+++ plugin/src/main/java/ru/easydonate/easypayments/service/KnownPlayersService.java
@@ -1,42 +1,58 @@
 package ru.easydonate.easypayments.service;
 
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.ref.WeakReference;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public final class KnownPlayersService {
 
-    private final @NotNull Set<String> joinedSinceLastDrain;
+    private final @NotNull Map<String, WeakReference<Player>> joinedSinceLastDrain;
     private final @NotNull Lock joinedSinceLastDrainLock;
 
     public KnownPlayersService() {
-        this.joinedSinceLastDrain = new HashSet<>();
+        this.joinedSinceLastDrain = new HashMap<>();
         this.joinedSinceLastDrainLock = new ReentrantLock();
     }
 
-    public @NotNull Set<String> drainJoinedPlayers() {
+    public @NotNull Map<String, Boolean> drainJoinedPlayers() {
+        Map<String, WeakReference<Player>> drained;
+
         try {
             joinedSinceLastDrainLock.lock();
 
             if (joinedSinceLastDrain.isEmpty())
-                return Collections.emptySet();
+                return Collections.emptyMap();
 
-            Set<String> drained = new HashSet<>(joinedSinceLastDrain);
+            drained = new HashMap<>(joinedSinceLastDrain);
             joinedSinceLastDrain.clear();
-            return drained;
         } finally {
             joinedSinceLastDrainLock.unlock();
         }
+
+        Map<String, Boolean> joinedPlayers = new HashMap<>();
+        drained.forEach((playerName, playerRef) -> {
+            boolean isOnline = false;
+            if (playerRef != null) {
+                Player player = playerRef.get();
+                if (player != null) {
+                    isOnline = player.isOnline();
+                }
+            }
+
+            joinedPlayers.put(playerName, isOnline);
+        });
+
+        return joinedPlayers;
     }
 
-    public void rememberJoinedPlayer(@NotNull String playerName) {
+    public void rememberJoinedPlayer(@NotNull Player player) {
         try {
             joinedSinceLastDrainLock.lock();
-            this.joinedSinceLastDrain.add(playerName);
+            this.joinedSinceLastDrain.put(player.getName(), new WeakReference<>(player));
         } finally {
             joinedSinceLastDrainLock.unlock();
         }
