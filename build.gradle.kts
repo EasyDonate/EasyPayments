9fc37ee7dd7c37d9ed17ce81f506ba25f5e54f8d
SoKnight <mcliyanage2018@gmail.com>
2026-01-28 23:23:04 +0500

    build: bump project version to 2.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.easydonate.easypayments"
-version = "2.5.2"
+version = "2.5.3"
 
 description = "The official payment processing implementation as alternative for the RCON protocol"
 
